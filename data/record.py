b06ba925931b75cdd6c355dab767c2130b6fb0fb
DrKarambit <43848302+DrKarambit@users.noreply.github.com>
2022-12-02 09:47:53 +0100

    Update record script

--- data/record.py
+++ data/record.py
@@ -6,7 +6,8 @@ import cv2
 device = int(sys.argv[1])
 res_x = int(sys.argv[2])
 res_y = int(sys.argv[3])
-output_path = sys.argv[4]
+sample_count = int(sys.argv[4])
+output_path = sys.argv[5]
 
 capture = cv2.VideoCapture(device)
 capture.set(3, res_x)
@@ -16,7 +17,7 @@ capture.set(cv2.CAP_PROP_AUTOFOCUS, 0)
 if output_path and not os.path.exists(output_path):
     os.makedirs(output_path)
 
-for i in range(320):
+for i in range(sample_count):
     frame = capture.read()[1]
     cv2.imshow('Frame', frame)
     cv2.imwrite(f'{output_path}/{i}.jpg', frame)
